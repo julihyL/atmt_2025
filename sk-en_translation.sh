dce5b9045ffde16f4b3a78111f00f4ad9c2d3e12
Huaiyue Luo <julijanaluo@gmail.com>
2025-10-05 20:18:18 +0200

    Update sk-en_translation.sh

--- sk-en_translation.sh
+++ sk-en_translation.sh
@@ -16,8 +16,8 @@ source activate atmt
 python preprocess.py \
   --source-lang sk \
   --target-lang en \
-  --raw-data sk-en/data/raw \
-  --dest-dir sk-en/data/prepared \
+  --raw-data . \
+  --dest-dir sk-en_data_prepared \
   --model-dir cz-en/tokenizers \
   --test-prefix train \
   --train-prefix train \
@@ -28,14 +28,14 @@ python preprocess.py \
 # === 2. Translate using trained Czech-English model ===
 python translate.py \
     --cuda \
-    --input sk-en/data/raw/train.sk \
+    --input train.sk \
     --src-tokenizer cz-en/tokenizers/cz-bpe-8000.model \
     --tgt-tokenizer cz-en/tokenizers/en-bpe-8000.model \
     --checkpoint-path cz-en/checkpoints/checkpoint_best.pt \
-    --output sk-en/output.txt \
+    --output output_sk_en.txt \
     --max-len 300
 
 # === 3. Evaluate BLEU score ===
 python evaluate.py \
-  --reference sk-en/data/raw/train.en \
-  --hypothesis sk-en/output.txt > sk-en/bleu_score.txt
+  --reference train.en \
+  --hypothesis output_sk_en.txt > bleu_score_sk_en.txt
